d56161c58abef82d9390d3bd3e5853f5c0a160e0
Mateusz Dec <mateusz.dec92@gmail.com>
2020-02-16 11:31:03 +0100

    zadanie z metod

--- src/metody_i_konstruktory4/CompuetrStore.java
+++ src/metody_i_konstruktory4/CompuetrStore.java
@@ -2,6 +2,20 @@ package metody_i_konstruktory4;
 
 public class CompuetrStore {
     public static void main(String[] args) {
-        
+
+        Computer comp1 = new Computer();
+        comp1.processor = "Intel i5";
+        comp1.memory = 8192;
+
+        Computer comp2 = new Computer();
+        comp2.processor = "AMD Ryzen 1700";
+        comp2.memory = 16_384;
+
+        comp1.printInfo();
+        comp2.printInfo();
+
+        System.out.println(comp1.getInfo());
+        String info2 = comp2.getInfo();
+        System.out.println(info2);
     }
 }
--- src/metody_i_konstruktory4/Computer.java
+++ src/metody_i_konstruktory4/Computer.java
@@ -3,4 +3,16 @@ package metody_i_konstruktory4;
 public class Computer {
     String processor;
     int memory;
+
+    void printInfo() {
+        System.out.println(processor + " " + memory);
+    }
+
+    String getInfo() {
+        String description = processor + " " + memory;
+        return description;
+
+        //lub
+//        return processor + " " + memory;
+    }
 }
