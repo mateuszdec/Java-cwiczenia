ea5eb5582935c6d72939d3acc07cc2a7a33d5a83
Mateusz Dec <mateusz.dec92@gmail.com>
2020-02-16 12:00:08 +0100

    Zadanie z komputerem

--- src/metody_i_konstruktory4/CompuetrStore.java
+++ src/metody_i_konstruktory4/CompuetrStore.java
@@ -3,19 +3,28 @@ package metody_i_konstruktory4;
 public class CompuetrStore {
     public static void main(String[] args) {
 
-        Computer comp1 = new Computer();
-        comp1.processor = "Intel i5";
-        comp1.memory = 8192;
+        ComputerFactory factory = new ComputerFactory();
+        Computer comp1 = factory.create("Intel i5", 8192);
+        Computer comp2 = factory.create("AMD Ryzen 1700", 16_384);
 
-        Computer comp2 = new Computer();
-        comp2.processor = "AMD Ryzen 1700";
-        comp2.memory = 16_384;
+
+//        Computer comp1 = new Computer();
+//        comp1.processor = "Intel i5";
+//        comp1.memory = 8192;
+
+//        Computer comp2 = new Computer();
+//        comp2.processor = "AMD Ryzen 1700";
+//        comp2.memory = 16_384;
 
         comp1.printInfo();
         comp2.printInfo();
+//
+//        System.out.println(comp1.getInfo());
+//        String info2 = comp2.getInfo();
+//        System.out.println(info2);
 
-        System.out.println(comp1.getInfo());
-        String info2 = comp2.getInfo();
-        System.out.println(info2);
+        ComputerUpgrade computerUpgrade = new ComputerUpgrade();
+        computerUpgrade.addMemory(comp1, 1024);
+        comp1.printInfo();
     }
 }
---
+++ src/metody_i_konstruktory4/ComputerUpgrade.java
@@ -0,0 +1,8 @@
+package metody_i_konstruktory4;
+
+public class ComputerUpgrade {
+
+    void addMemory(Computer computer, int additionalMemory) {
+        computer.memory = computer.memory + additionalMemory;
+    }
+}
