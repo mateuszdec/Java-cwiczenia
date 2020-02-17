bd4dd227e42eedf4282c546153d52c0db349676b
Mateusz Dec <mateusz.dec92@gmail.com>
2020-02-17 21:39:54 +0100

    Przeciążanie metod i this - zadanie

--- src/przeciążanie_metod_i_this5/zadanie/CalcTest.java
+++ src/przeciążanie_metod_i_this5/zadanie/CalcTest.java
@@ -1,4 +1,7 @@
 package przeciążanie_metod_i_this5.zadanie;
 
 public class CalcTest {
+    public static void main(String[] args) {
+        
+    }
 }
--- src/przeciążanie_metod_i_this5/zadanie/Calculator.java
+++ src/przeciążanie_metod_i_this5/zadanie/Calculator.java
@@ -1,4 +1,20 @@
 package przeciążanie_metod_i_this5.zadanie;
 
 public class Calculator {
+
+    double add(double a, double b) {
+        return a + b;
+    }
+
+    double add(double a, double b, double c) {
+        return a + b + c;
+    }
+
+    double subtract(double a, double b) {
+        return a - b;
+    }
+
+    double subtract(double a, double b, double c) {
+        return a - b - c;
+    }
 }
