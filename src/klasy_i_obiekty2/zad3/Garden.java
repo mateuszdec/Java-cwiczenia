1754494114d908af11f9425208c91a8fa454af47
Mateusz Dec <mateusz.dec92@gmail.com>
2020-04-02 13:17:55 +0200

    Klasy i obiekty zad 3

---
+++ src/klasy_i_obiekty2/zad3/Garden.java
@@ -0,0 +1,4 @@
+package klasy_i_obiekty2.zad3;
+
+public class Garden {
+}
---
+++ src/klasy_i_obiekty2/zad3/Plant.java
@@ -0,0 +1,7 @@
+package klasy_i_obiekty2.zad3;
+
+public class Plant {
+    String name;
+    String description;
+    double waterConsumption;
+}
