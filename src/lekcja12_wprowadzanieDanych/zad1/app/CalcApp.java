e06cbf9a66ab55b06784ec9d1aeb172c6452450b
Mateusz Dec <mateusz.dec92@gmail.com>
2020-04-09 17:04:09 +0200

    wprowadzanie danych zad 1

---
+++ src/lekcja12_wprowadzanieDanych/zad1/app/CalcApp.java
@@ -0,0 +1,24 @@
+package lekcja12_wprowadzanieDanych.zad1.app;
+
+import lekcja12_wprowadzanieDanych.zad1.logic.Calculator;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class CalcApp {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        input.useLocale(Locale.US);
+
+        double a = input.nextDouble();
+        input.nextLine();
+        String operator = input.nextLine();
+        double b = input.nextDouble();
+
+        Calculator calc = new Calculator();
+        double result = calc.calculate(a, b, operator);
+        System.out.println(a + operator + b + " = " + result);
+
+        input.close();
+    }
+}
