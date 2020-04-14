b025e6b40fb96cfb4fdd68f7fbd2217b302d8a0d
Mateusz Dec <mateusz.dec92@gmail.com>
2020-04-14 12:14:59 +0200

    Pętle zad1

---
+++ src/lekcja13_pętle/zad1/SumNumbers.java
@@ -0,0 +1,21 @@
+package lekcja13_pętle.zad1;
+
+import java.util.Scanner;
+
+public class SumNumbers {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Podaj ile liczb chcesz zsumować: ");
+        int numbers = sc.nextInt();
+
+        int sum = 0;
+        while(numbers-- > 0) {
+            System.out.println("Podaj kolejną liczbę: ");
+            sum = sum + sc.nextInt();
+        }
+
+        System.out.println("Suma wszystkich podanych liczb to: " + sum);
+        sc.close();
+    }
+}
