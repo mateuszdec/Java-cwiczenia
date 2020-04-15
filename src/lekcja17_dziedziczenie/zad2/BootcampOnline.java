6ef146f85b3299f8ec9f3b2bbbc1b73845b7779f
Mateusz Dec <mateusz.dec92@gmail.com>
2020-04-15 15:26:20 +0200

    dziedziczenie zad2

---
+++ src/lekcja17_dziedziczenie/zad2/BootcampOnline.java
@@ -0,0 +1,22 @@
+package lekcja17_dziedziczenie.zad2;
+
+public class BootcampOnline extends OnlineCourse {
+    private String teacher;
+    private int consultationHours;
+
+    public String getTeacher() {
+        return teacher;
+    }
+
+    public void setTeacher(String teacher) {
+        this.teacher = teacher;
+    }
+
+    public int getConsultationHours() {
+        return consultationHours;
+    }
+
+    public void setConsultationHours(int consultationHours) {
+        this.consultationHours = consultationHours;
+    }
+}
---
+++ src/lekcja17_dziedziczenie/zad2/OnlineCourse.java
@@ -0,0 +1,22 @@
+package lekcja17_dziedziczenie.zad2;
+
+public class OnlineCourse extends Course {
+    private int videoLengthMin;
+    private int timeToFinish;
+
+    public int getVideoLengthMin() {
+        return videoLengthMin;
+    }
+
+    public void setVideoLengthMin(int videoLengthMin) {
+        this.videoLengthMin = videoLengthMin;
+    }
+
+    public int getTimeToFinish() {
+        return timeToFinish;
+    }
+
+    public void setTimeToFinish(int timeToFinish) {
+        this.timeToFinish = timeToFinish;
+    }
+}
