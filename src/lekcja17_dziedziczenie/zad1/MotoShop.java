2e408b3baaa6d277eab2357c610409a475d6ab4f
Mateusz Dec <mateusz.dec92@gmail.com>
2020-04-15 15:17:54 +0200

    dziedziczenie zad1

---
+++ src/lekcja17_dziedziczenie/zad1/MotoShop.java
@@ -0,0 +1,12 @@
+package lekcja17_dziedziczenie.zad1;
+
+public class MotoShop {
+    public static void main(String[] args) {
+        Tire tire = new Tire(123, "SuperTire", "ProContact", "Winter pro", 17, 225);
+        tire.printInfo();
+
+        ExhaustPart exhaustPart = new ExhaustPart(987, "Noisy Parts", "Turbo Brum", "Noisy Exhaust", true);
+        exhaustPart.printInfo();
+
+    }
+}
