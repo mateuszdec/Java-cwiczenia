4f3530ac7dd86cb70e29093714cf235e30a0602e
Mateusz Dec <mateusz.dec92@gmail.com>
2020-05-09 18:55:36 +0200

    Pilimorfizm zad 1

---
+++ src/lekcja19_polimorfizm/zad1/Nurse.java
@@ -0,0 +1,19 @@
+package lekcja19_polimorfizm.zad1;
+
+class Nurse extends Person {
+
+    private double overtime;
+
+    public Nurse(String firstName, String lastName, double salary, double overtime) {
+        super(firstName, lastName, salary);
+        this.overtime = overtime;
+    }
+
+    public double getOvertime() {
+        return overtime;
+    }
+
+    public void setOvertime(double overtime) {
+        this.overtime = overtime;
+    }
+}
