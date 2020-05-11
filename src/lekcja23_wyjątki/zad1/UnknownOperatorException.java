deb3d00dfed5e45c4356715a54108c025007fd0d
Mateusz Dec <mateusz.dec92@gmail.com>
2020-05-11 18:55:26 +0200

    Wyjątki zad 1

---
+++ src/lekcja23_wyjątki/zad1/UnknownOperatorException.java
@@ -0,0 +1,7 @@
+package lekcja23_wyjątki.zad1;
+
+public class UnknownOperatorException extends RuntimeException {
+    public UnknownOperatorException(String message) {
+        super(message);
+    }
+}
