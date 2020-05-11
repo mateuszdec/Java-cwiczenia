916bd22652fcada90496037a921cf7bf96e52295
Mateusz Dec <mateusz.dec92@gmail.com>
2020-05-11 19:07:31 +0200

    początek zad 2

---
+++ src/lekcja23_wyjątki/zad2/Competition.java
@@ -0,0 +1,38 @@
+package lekcja23_wyjątki.zad2;
+
+public class Competition {
+    private String name;
+    private Participant[] participants;
+    private int ageLimit;
+    private int size;
+
+    public Competition(String name, int maxParticipants, int ageLimit) {
+        this.name = name;
+        this.ageLimit = ageLimit;
+        this.participants = new Participant[maxParticipants];
+    }
+
+    void addParticipant(Participant participant) {
+        participants[size] = participant;
+        size++;
+    }
+
+    boolean hasFreeSpots() {
+        return size < participants.length;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder builder = new StringBuilder();
+        builder.append("Zawody ")
+                .append(name)
+                .append("\n")
+                .append("Liczba uczestników: ").append(size);
+        for (int i = 0; i < size; i++) {
+            builder.append("\n");
+            builder.append(" > ");
+            builder.append(participants[i].toString());
+        }
+        return builder.toString();
+    }
+}
---
+++ src/lekcja23_wyjątki/zad2/CompetitionApp.java
@@ -0,0 +1,8 @@
+package lekcja23_wyjątki.zad2;
+
+public class CompetitionApp {
+    public static void main(String[] args) {
+        CompetitionController competitionController = new CompetitionController();
+        competitionController.run();
+    }
+}
