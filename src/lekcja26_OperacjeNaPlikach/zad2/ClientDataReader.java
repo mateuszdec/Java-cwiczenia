70eda9b1bb4bbca2a06a3aabd45df1b3e6851c8e
Mateusz Dec <mateusz.dec92@gmail.com>
2020-05-29 11:16:26 +0200

    Operacje na plikach zad2

---
+++ src/lekcja26_OperacjeNaPlikach/zad2/ClientDataReader.java
@@ -0,0 +1,42 @@
+package lekcja26_OperacjeNaPlikach.zad2;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+class ClientDataReader {
+    static Client[] readFile(String fileName) throws FileNotFoundException {
+        final int linesNumber = countLinesWithoutHeader(fileName);
+        Client[] clients = new Client[linesNumber];
+        try (Scanner scanner = new Scanner(new File(fileName))) {
+            scanner.nextLine();
+            for (int i = 0; i < clients.length; i++) {
+                String csvLine = scanner.nextLine();
+                clients[i] = createClientFromCsv(csvLine);
+            }
+        }
+        return clients;
+    }
+
+    private static Client createClientFromCsv(String csvLine) {
+        String[] data = csvLine.split(",");
+        int id = Integer.parseInt(data[0]);
+        String firstName = data[1];
+        String lastName = data[2];
+        String country = data[3];
+        double value = Double.parseDouble(data[4]);
+        return new Client(id, firstName, lastName, country, value);
+    }
+
+    private static int countLinesWithoutHeader(String fileName) throws FileNotFoundException {
+        int lines = 0;
+        try (Scanner scanner = new Scanner(new File(fileName))) {
+            scanner.nextLine();
+            while (scanner.hasNextLine()) {
+                scanner.nextLine();
+                lines++;
+            }
+        }
+        return 0;
+    }
+}
