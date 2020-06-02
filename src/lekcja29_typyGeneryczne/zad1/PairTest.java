e3493bc4e45f7a38623fc6e36f23f4e227a1c0aa
Mateusz Dec <mateusz.dec92@gmail.com>
2020-06-02 10:43:28 +0200

    Typy generyczne zad1

--- src/lekcja29_typyGeneryczne/zad1/PairTest.java
+++ src/lekcja29_typyGeneryczne/zad1/PairTest.java
@@ -4,11 +4,17 @@ public class PairTest {
     public static void main(String[] args) {
         Pair<String, String> pair1 = new Pair<>("Ania", "Kowalska");
         Pair<Double, Double> pair2 = new Pair<>(5.5, 10.5);
+        Pair<Integer, String> pair3 = new Pair<>(12345, "Jan Kowalski");
 
-        System.out.println(pair1.getT() + " " + pair1.getV());
+//        System.out.println(pair1.getT() + " " + pair1.getV());
+        printPair(pair1);
         double sum = pair2.getT() + pair2.getV();
+        printPair(pair2);
         System.out.println(sum);
+        printPair(pair3);
     }
 
-
+    static <T, V> void printPair(Pair<T, V> pair) {
+        System.out.println(pair.getT() + " " + pair.getV());
+    }
 }
