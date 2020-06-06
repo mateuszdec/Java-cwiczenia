dfb56aa5985aa5259def75ac7283c32afa4fa944
Mateusz Dec <mateusz.dec92@gmail.com>
2020-06-06 10:56:22 +0200

    zbiory zad2

---
+++ src/lekcja33_zbiory/zad2/ProductMain.java
@@ -0,0 +1,8 @@
+package lekcja33_zbiory.zad2;
+
+public class ProductMain {
+    public static void main(String[] args) {
+        ProductManager productManager = new ProductManager();
+        productManager.mainLoop();
+    }
+}
