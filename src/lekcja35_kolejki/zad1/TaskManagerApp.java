dac50a8aadd5b29d80b29d4be08892921014ba81
Mateusz Dec <mateusz.dec92@gmail.com>
2020-06-08 11:34:54 +0200

    Kolejki zad1

---
+++ src/lekcja35_kolejki/zad1/TaskManagerApp.java
@@ -0,0 +1,8 @@
+package lekcja35_kolejki.zad1;
+
+public class TaskManagerApp {
+    public static void main(String[] args) {
+        TaskManager taskManager = new TaskManager();
+        taskManager.mainLoop();
+    }
+}
