5c569851bd466d449a20d0595b4747e5e549379a
Mateusz Dec <mateusz.dec92@gmail.com>
2020-06-08 19:23:22 +0200

    Iteratory zad1

---
+++ src/lekcja36_iteratory/zad1/NamesIterator.java
@@ -0,0 +1,24 @@
+package lekcja36_iteratory.zad1;
+
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class NamesIterator {
+    public static void main(String[] args) {
+        Map<String, Person> people = new TreeMap<>();
+
+        people.put("Kowalski", new Person("Jan", "Kowalski", 35));
+        people.put("Adamiak", new Person("Ania", "Adamiak", 21));
+        people.put("Zaręba", new Person("Adam", "Zaręba", 17));
+        people.put("Piotrowski", new Person("Karol", "Piotrowski", 42));
+        people.put("Bobrowska", new Person("Michalina", "Bobrowska", 15));
+
+        Collection<Person> values = people.values();
+        Iterator<Person> iterator = values.iterator();
+        while (iterator.hasNext()) {
+            System.out.println(iterator.next());
+        }
+    }
+}
