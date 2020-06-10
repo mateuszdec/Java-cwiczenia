a36e573ffb3f6e4c01b33e871a748306b5ee47d5
Mateusz Dec <mateusz.dec92@gmail.com>
2020-06-10 08:45:37 +0200

    Wyrażenia lambda zad2

---
+++ src/lekcja38_wyrażeniaLambda/zad2/EmailManager.java
@@ -0,0 +1,54 @@
+package lekcja38_wyrażeniaLambda.zad2;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+
+public class EmailManager {
+    public static void main(String[] args) {
+        List<Email> emailList = createEmailList();
+        //tylko wysłane emaile
+        List<Email> sentEmails = filter(emailList, email -> email.isSent());
+        System.out.println("Wysłane maile:");
+        System.out.println(sentEmails);
+        //filtrowanie maili, w których nadawca lub odbiorca ma wskazany adres email
+        List<Email> bartEmails = filter(emailList, email -> email.isSenderOrRecipient("bart@example.com"));
+        System.out.println("Maile przefiltrowane na podstawie nadawcy lub odbiorcy");
+        System.out.println(bartEmails);
+    }
+
+    private static List<Email> filter(List<Email> emails, Predicate<Email> predicate) {
+        List<Email> filtereEmails = new ArrayList<>();
+        for (Email email : emails) {
+            if (predicate.test(email))
+                filtereEmails.add(email);
+        }
+        return filtereEmails;
+    }
+
+    private static List<Email> createEmailList() {
+        List<Email> emails = new ArrayList<>();
+        emails.add(new Email(
+                "joe@example.com",
+                "barbara@example.com",
+                "Kup bułki",
+                "Cześć!, Kup proszę bułki, gdy będziesz wracać z pracy.",
+                false)
+        );
+        emails.add(new Email(
+                "carl@example.com",
+                "joe@example.com",
+                "Nowa inwestycja",
+                "Siema! Musimy omówić temat nowej inwestycji, pasuje Ci jutro?",
+                true)
+        );
+        emails.add(new Email(
+                "joe@example.com",
+                "bart@example.com",
+                "Wypad na miasto",
+                "Cześć. Idziemy dzisiaj wieczorem z chłopakami na miasto. Dołączasz do nas?",
+                true)
+        );
+        return emails;
+    }
+}
