721830d7b036ea30b11940ddcbea3a6eb2c58e4d
Mateusz Dec <mateusz.dec92@gmail.com>
2020-06-11 08:54:58 +0200

    Referencje do metod zad1

---
+++ src/lekcja39_referencjeDoMetod/SimpleStringComparator.java
@@ -0,0 +1,13 @@
+package lekcja39_referencjeDoMetod;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class SimpleStringComparator {
+    public static void main(String[] args) {
+        List<String> names = Arrays.asList("Karol", "Wojtek", "Alicja", "Bartek", "Romek");
+        names.sort(String::compareToIgnoreCase);
+        names.forEach(System.out::println);
+    }
+}
