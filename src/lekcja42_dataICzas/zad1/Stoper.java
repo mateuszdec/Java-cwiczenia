7bd461e28aeb47c7962167ccb0095da4f8921aef
Mateusz Dec <mateusz.dec92@gmail.com>
2020-06-14 13:58:41 +0200

    data i czas zad1

---
+++ src/lekcja42_dataICzas/zad1/Stoper.java
@@ -0,0 +1,22 @@
+package lekcja42_dataICzas.zad1;
+
+import java.time.Duration;
+import java.time.Instant;
+import java.util.Scanner;
+
+public class Stoper {
+    public static void main(String[] args) {
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.println("Żeby rozpocząć pomiar czasu wciśnij ENTER");
+            sc.nextLine();
+            Instant i1 = Instant.now();
+
+            System.out.println("Żeby zatrzymać pomiar czasu wciśnij ENTER");
+            sc.nextLine();
+            Instant i2 = Instant.now();
+
+            Duration time = Duration.between(i1, i2);
+            System.out.println("Czas, który upłynął: " + time.getSeconds());
+        }
+    }
+}
