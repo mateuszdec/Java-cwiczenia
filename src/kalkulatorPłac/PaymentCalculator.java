d0e9350b70f41384c77fea67a6a58982af0e8fa3
Mateusz Dec <mateusz.dec92@gmail.com>
2020-06-16 23:58:37 +0200

    zadanie kalkulator płac

---
+++ src/kalkulatorPłac/PaymentCalculator.java
@@ -0,0 +1,15 @@
+package kalkulatorPłac;
+
+public class PaymentCalculator {
+    private final static double MONTHLY_ZUS = 1000;
+
+    public static double nettoYearPayment(Employee employee) {
+        return employee.getSalary() * 12;
+    }
+
+    public static double bruttoYearPayment(Employee employee) {
+        double nettoSalary = employee.getSalary();
+        double bruttoSalary = nettoSalary * 0.2;
+        return 12 * (nettoSalary  + bruttoSalary + MONTHLY_ZUS);
+    }
+}
---
+++ src/kalkulatorPłac/PaymentTest.java
@@ -0,0 +1,11 @@
+package kalkulatorPłac;
+
+public class PaymentTest {
+    public static void main(String[] args) {
+        Employee employee = new Employee("Jan", "Kowalski", 3000);
+        double nettoYearPayment = PaymentCalculator.nettoYearPayment(employee);
+        double bruttoYearPayment = PaymentCalculator.bruttoYearPayment(employee);
+        System.out.println("Roczna wypłata netto: " + nettoYearPayment);
+        System.out.println("Roczna wypłata brutto: " + bruttoYearPayment);
+    }
+}
