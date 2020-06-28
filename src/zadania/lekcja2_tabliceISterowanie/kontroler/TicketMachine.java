f555ac25857456fbc467c9e023af894fe6eb3287
Mateusz Dec <mateusz.dec92@gmail.com>
2020-06-28 20:13:26 +0200

    zadanie

---
+++ src/zadania/lekcja2_tabliceISterowanie/kontroler/TicketMachine.java
@@ -0,0 +1,8 @@
+package zadania.lekcja2_tabliceISterowanie.kontroler;
+
+public class TicketMachine {
+    public static void buyTicket(Passenger passenger, int timeMinutes) {
+        Ticket ticket = new Ticket(timeMinutes);
+        passenger.setTicket(ticket);
+    }
+}
