442e4d92e689276fa2a20a0133146f5568a1d5b2
Mateusz Dec <mateusz.dec92@gmail.com>
2020-06-29 20:26:24 +0200

    zadanie

---
+++ src/zadania/lekcja2_tabliceISterowanie/wisielec/Main.java
@@ -0,0 +1,8 @@
+package zadania.lekcja2_tabliceISterowanie.wisielec;
+
+public class Main {
+    public static void main(String[] args) {
+        Game game = new Game();
+        game.mainLoop();
+    }
+}
