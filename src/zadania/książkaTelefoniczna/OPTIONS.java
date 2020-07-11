578475ae41e8fee3d61793823d7076948fe1d44a
Mateusz Dec <mateusz.dec92@gmail.com>
2020-07-11 09:21:35 +0200

    zadanie

---
+++ src/zadania/książkaTelefoniczna/OPTIONS.java
@@ -0,0 +1,34 @@
+package zadania.książkaTelefoniczna;
+
+import java.util.NoSuchElementException;
+
+public enum OPTIONS {
+    ADD_CONTACT(0, "Dodaj kontakt"),
+    SEARCH_BY_NAME(1, "Szukaj po nazwie"),
+    SEARCH_BY_TEL(2, "Szukaj po telefonie"),
+    DELETE(3, "Usuń"),
+    CLOSE(4, "Koniec");
+
+    private final int shortcut;
+    private final String description;
+
+    OPTIONS(int shortcut, String description) {
+        this.shortcut = shortcut;
+        this.description = description;
+    }
+
+    public int getShortcut() {
+        return shortcut;
+    }
+
+    static OPTIONS convertToOption(int option) {
+        if(option >= values().length || option < 0)
+            throw new NoSuchElementException();
+        return values()[option];
+    }
+
+    @Override
+    public String toString() {
+        return shortcut + " - " + description;
+    }
+}
---
+++ src/zadania/książkaTelefoniczna/TeleBookApp.java
@@ -0,0 +1,8 @@
+package zadania.książkaTelefoniczna;
+
+public class TeleBookApp {
+    public static void main(String[] args) {
+        TeleBookController controller = new TeleBookController();
+        controller.loop();
+    }
+}
